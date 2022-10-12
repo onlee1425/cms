5044f42e03fc657ae4e44e4c5c884e7d62059d00
onlee1425 <enflenffl@gmail.com>
2022-10-12 21:55:48 +0900

    feat : [011-PRODUCT-MODIFY] Product, ProductItem Modify

--- order-api/src/main/java/com/zb/cms/order/exception/ErrorCode.java
+++ order-api/src/main/java/com/zb/cms/order/exception/ErrorCode.java
@@ -9,7 +9,9 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     NOT_FOUND_PRODUCT(HttpStatus.BAD_REQUEST,"상품을 찾을 수 없습니다."),
 
-    SAME_ITEM_NAME(HttpStatus.BAD_REQUEST,"동일한 이름의 아이템이 이미 존재합니다.");
+    SAME_ITEM_NAME(HttpStatus.BAD_REQUEST,"동일한 이름의 아이템이 이미 존재합니다."),
+
+    NOT_FOUNT_ITEM(HttpStatus.BAD_REQUEST,"아이템을 찾을 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String detail;
